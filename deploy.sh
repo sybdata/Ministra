eb5d33ee583c3c61e79fc709d2fb0bf30be2f25b
sybdata <as15@wt.de>
2020-02-15 19:18:42 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+STALKER="/var/www/stalker_portal"
+
+if [ ! -d $STALKER/deploy ]; then
+        wget -O /tmp/stalker.zip https://sybdata.de/filebrowser/ministra-5.6.1.zip
+        unzip /tmp/stalker.zip -d /var/www/
+        mv /var/www/stalker_portal-*/* /var/www/stalker_portal/
+        rm -rf /var/www/stalker_portal-*
+fi
+
+if [ ! -s $STALKER/server/custom.ini ]; then
+        wget -O $STALKER/server/custom.ini https://gist.githubusercontent.com/klyushkov/2dcec9f537458ff702cddf882e12eca5/raw/f3dc2c1a8d7a54d9710ba4d650a7a01e9da9a9b2/stalker_custom.ini
+fi
+
+sed -i '/\bmysql_tzinfo_to_sql\b/g' $STALKER/deploy/build.xml
+
+cd $STALKER/deploy && phing
+if [ $? -eq 1 ]; then
+        phing
+fi
+
+echo Success!
